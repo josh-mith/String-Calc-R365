40caee6260fb7cdd6c0ba0038e7dd2e8e9ea3abd
josh-mith <53532738+josh-mith@users.noreply.github.com>
2021-12-24 12:50:18 -0500

    First commit- Step 1
    Step 1- Add function and corresponding unit tests

---
+++ StringCalcNewSLN/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalcNewSLN
+{
+    public class Program
+    {
+        private const int I_STARTING_NUM = 0;
+
+        //Function to add user-inputted numbers
+        public static int Add(string sInput)
+        {
+            var aNumbers = sInput.Split(",");
+            var iSum = 0;
+
+            if (aNumbers.Length > 2)
+            {
+                throw new Exception("You cannot add more than 2 numbers in your sum.");
+            }
+
+            foreach (var cNumber in aNumbers)
+            {
+                if (int.TryParse(cNumber, out int iCleanedNumber))
+                {
+                    iSum += iCleanedNumber;
+                }
+            }
+            return iSum;
+        }
+        public static void Main(string[] args)
+        {
+            //Prompt user for input
+            Console.WriteLine("Which numbers would you like find the sum of?");
+
+            //Collect user input
+            string sUserInput = Console.ReadLine();
+
+            //Run Add function w/ user input
+            Console.WriteLine(Add(sUserInput));
+
+
+        }
+    }
+}
---
+++ StringCalcTest/Objective1Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalcNewSLN;
+
+namespace StringCalcNewSLNTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("26, 17", 43)]
+        [TestCase("21, 14", 35)]
+        public void ReturnsSumOfCommaDelimitedNumbers(string sInput, int iRealResult)
+        {
+            Program programTest = new StringCalcNewSLN.Program();
+            int iCalculatedResult = Program.Add(sInput);
+            Assert.AreEqual(iCalculatedResult, iRealResult);
+        }
+
+        [Test]
+        [TestCase("28,", 28)]
+        [TestCase("", 0)]
+        [TestCase("714,Irvine", 714)]
+        public void RegardsEmptyTextAndNonNumericAsZero(string sInput, int iRealResult)
+        {
+            Program programTest = new StringCalcNewSLN.Program();
+            int iCalculatedResult = Program.Add(sInput);
+            Assert.AreEqual(iCalculatedResult, iRealResult);
+        }
+
+        [Test]
+        [TestCase("2,4,6,8")]
+        [TestCase("19,appreciate,20")]
+        public void ShouldAllowSumOfMaximumTwoNumbers(string sInput)
+        {
+            Program programTest = new StringCalcNewSLN.Program();
+            Assert.Throws<Exception>(() => Program.Add(sInput));
+        }
+    }
+}
